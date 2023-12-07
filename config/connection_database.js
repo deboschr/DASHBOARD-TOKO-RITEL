9b0e8525ed98f5fdfcdf0e7f6f8b7ddcfb20c9db
cornel <135084170+robot-afk@users.noreply.github.com>
2023-12-08 02:02:55 +0700

    update

--- config/connection_database.js
+++ config/connection_database.js
@@ -1,18 +1,23 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("tubes_manpro", "root", "", {
-	host: "localhost",
-	dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  "hmpg7764_manpro_tubes_jack",
+  "hmpg7764_manpro_jack",
+  "manpro_jack",
+  {
+    host: "hmpsars.id",
+    dialect: "mysql",
+  }
+);
 
 // Coba koneksi ke database
 (async () => {
-	try {
-		await sequelize.authenticate();
-		console.log("Connection to database has been established successfully.");
-	} catch (error) {
-		console.error("Unable to connect to the database:", error);
-	}
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to database has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 })();
 
 export { sequelize };
